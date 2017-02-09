08ea740aa32c1941b756503c7682265a98206c08
MarinaRuizSO <marina_dorada@hotmail.es>
2017-02-09 13:57:58 +0000

    Add files via upload

--- LJtest.py
+++ LJtest.py
@@ -7,8 +7,9 @@ from Particle3D import Particle3D as P
 import numpy as np
 import math
 import sys
-import MIC as mic
+from MIC import vecsep
 import LennardJones as LJ
+
 
 #Read name of input file
 if len(sys.argv)!=5:
@@ -18,7 +19,9 @@ else:
     infile1 = sys.argv[1] #particle1
     infile2 = sys.argv[2] #particle2
     L = float(sys.argv[3]) #box length
+"""
     r_c = float(sys.argv[4]) #cutoff radius
+"""
 #Create particles
 p1_file = open(infile1, "r")
 p1 = P.createparticle(p1_file)
--- LennardJones.py
+++ LennardJones.py
@@ -10,35 +10,35 @@ import sys
 import math
 import matplotlib.pyplot as pyplot
 import numpy as np
-from Particle3D import Particle3D as p3d
-import MIC as mic
+from Particle3D import Particle3D as P
+from MIC import vecsep 
 
-
+print vecsep
 # Calculates the force between two particles based on the separation, the cutoff radius and the box dimensions 
-
-def ljforce (p1,p2,L,r_c) :
+"""
+def ljforce (p1,p2,L,r_c):
     # Computes the vector separation of the particles
     rvector = mic.vecsep(p1,p2)
     # Computes the magnitue of the particle separation 
     rscalar = math.sqrt(np.inner(rvector,rvector))
-        if rscalar > r_c :
-	    return ljforce == 0
-        else : 
-	    return (48*((1/rscalar**14)-(1/(2*rscalar**8))))*rvector
+    if rscalar > r_c:
+        return 0
+    else: 
+        return (48*((1/rscalar**14)-(1/(2*rscalar**8))))*rvector
 
 # Calculates the Lennard Jones potential for the particle pair. 			
-def ljpotential(p1,p2,L,r_c) :
+def ljpotential(p1,p2,L,r_c):
     # Computes the vector separation of the particles
     rvector = mic.vecsep(p1,p2)
     # Computes the magnitude of the particle separation 
     rscalar = math.sqrt(np.inner(rvector,rvector)
-        if rscalar > r_c :
-	    return ljpotential == 0
-        else : 
-	    return (4*((1/rscalar**12)-(1/(r**6))))*rvector
+    if rscalar > r_c:
+        return 0
+    else: 
+	return (4*((1/rscalar**12)-(1/(rscalar**6))))*rvector
 
 
-
+"""
 	
 
  
